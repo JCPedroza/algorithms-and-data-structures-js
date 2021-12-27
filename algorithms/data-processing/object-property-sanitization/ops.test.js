2fe3eb562c3eddd9f0ae3df365d4948921ae99a7
JCPedroza <nemesis4go10@hotmail.com>
2021-12-27 00:06:31 -0600

    refactor: use 'algorithm' as export name, instead of 'fun'

--- algorithms/data-processing/object-property-sanitization/ops.test.js
+++ algorithms/data-processing/object-property-sanitization/ops.test.js
@@ -1,17 +1,40 @@
 const testSubjects = [
-  require('./ops-for-each'),
-  require('./ops-reduce'),
+  require('./ops-assign-proxy'),
+  require('./ops-entries-filter-map'),
   require('./ops-filter-reduce'),
-  require('./ops-entries-filter-map')
+  require('./ops-for-each'),
+  require('./ops-reduce')
 ]
 
-testSubjects.forEach(({ fun, id }) => {
-  test(`${id} sanitizes properly`, () => {
+testSubjects.forEach(({ algorithm, id }) => {
+  test(`${id} sanitizes a three-property object`, () => {
     const object = { title: 'One', year: 2021, hack: true }
     const properties = ['title', 'year']
-    const sanitizedCopy = fun(object, properties)
+    const sanitizedCopy = algorithm(object, properties)
 
     // Must not have the 'hack' property
     expect(sanitizedCopy).toEqual({ title: 'One', year: 2021 })
+  })
+})
+
+testSubjects.forEach(({ algorithm, id }) => {
+  test(`${id} sanitizes an empty object`, () => {
+    const object = {}
+    const properties = ['title', 'year']
+    const sanitizedCopy = algorithm(object, properties)
+
+    // Sanitizing an empty object must return an empty object
+    expect(sanitizedCopy).toEqual({})
+  })
+})
+
+testSubjects.forEach(({ algorithm, id }) => {
+  test(`${id} sanitizes using empty properties array`, () => {
+    const object = { title: 'One', year: 2021, hack: true }
+    const properties = []
+    const sanitizedCopy = algorithm(object, properties)
+
+    // Sanitizing using an empty properties array must return an empty object
+    expect(sanitizedCopy).toEqual({})
   })
 })
