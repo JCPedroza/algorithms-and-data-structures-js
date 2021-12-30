28679765aeaa7dca495f0ea627a06d6f115b069c
JCPedroza <nemesis4go10@hotmail.com>
2021-12-30 06:17:19 -0600

    refactor: change variable name from 'io' to 'testCases'

--- algorithms/sequences-and-collections/fizz-buzz/fizz-buzz-range.test.js
+++ algorithms/sequences-and-collections/fizz-buzz/fizz-buzz-range.test.js
@@ -3,7 +3,7 @@ const testSubjects = [
   require('./fizz-buzz-range-map')
 ]
 
-const io = {
+const testCases = {
   scenarioOne: {
     in: [12, 20],
     out: ['fizz', '13', '14', 'fizzbuzz', '16', '17', 'fizz', '19', 'buzz']
@@ -17,7 +17,7 @@ const io = {
 
 testSubjects.forEach(({ algorithm, id }) => {
   test(`"${id}" correctly generates fizzbuzz ranges`, () => {
-    Object.values(io).forEach(scenario => {
+    Object.values(testCases).forEach(scenario => {
       expect(algorithm(...scenario.in)).toEqual(scenario.out)
     })
   })
