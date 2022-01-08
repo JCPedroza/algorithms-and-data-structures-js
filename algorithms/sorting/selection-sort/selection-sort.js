8f64ae7b8fd2bc7173d2982b4d161d3c351f0503
JCPedroza <nemesis4go10@hotmail.com>
2022-01-08 13:35:47 -0600

    refactor: use one variable (instead of two) to keep track of lowest value

--- algorithms/sorting/selection-sort/selection-sort.js
+++ algorithms/sorting/selection-sort/selection-sort.js
@@ -1,23 +1,22 @@
 /**
  * Sorts a numeric array in-place using the Selection Sort approach.
- * Time complexity: O(n^2) for best, worst, average
- * Space complexity: total O(n) auxiliary O(1)
+ * Time complexity: O(n^2) for best, worst, and average.
+ * Space complexity: total O(n) auxiliary O(1).
  * @param {number[]} array Array to be sorted.
  * @returns {number[]} Sorted array.
  */
 const selectionSort = (array) => {
   for (let pivot = 0; pivot < array.length - 1; pivot++) {
-    let minValue = Infinity
-    let minIndex = Infinity
+    let minValueIndex = pivot
 
-    for (let target = pivot; target < array.length; target++) {
-      if (array[target] < minValue) {
-        minIndex = target
-        minValue = array[target]
+    // Find minimum value and swap it with the pivot
+    for (let target = pivot + 1; target < array.length; target++) {
+      if (array[target] < array[minValueIndex]) {
+        minValueIndex = target
       }
     }
 
-    [array[pivot], array[minIndex]] = [array[minIndex], array[pivot]]
+    [array[pivot], array[minValueIndex]] = [array[minValueIndex], array[pivot]]
   }
 
   return array
