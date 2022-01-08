f10e4c55613c37693cf1915e2b8a161aea354e98
JCPedroza <nemesis4go10@hotmail.com>
2022-01-08 13:34:45 -0600

    refactor: change variable name from index to pivot

--- algorithms/sorting/insertion-sort/insertion-sort.js
+++ algorithms/sorting/insertion-sort/insertion-sort.js
@@ -1,17 +1,18 @@
 /**
  * Sorts a numeric array in-place using the Insertion Sort approach.
- * Time complexity: O(n^2) for best, worst, and average
- * Space complexity: total O(n) auxiliary O(1)
+ * Time complexity: best O(n) worst O(n^2) average O(n^2).
+ * Space complexity: total O(n) auxiliary O(1).
  * @param {number[]} array Array to be sorted.
  * @returns {number[]} Sorted array.
  */
 const insertionSort = (array) => {
-  for (let round = 0; round < array.length; round++) {
-    let index = round
+  for (let round = 1; round < array.length; round++) {
+    let pivot = round
 
-    while (index > 0 && array[index - 1] > array[index]) {
-      [array[index - 1], array[index]] = [array[index], array[index - 1]]
-      index--
+    // Swap with left value until a lower value is found
+    while (pivot > 0 && array[pivot - 1] > array[pivot]) {
+      [array[pivot - 1], array[pivot]] = [array[pivot], array[pivot - 1]]
+      pivot--
     }
   }
 
