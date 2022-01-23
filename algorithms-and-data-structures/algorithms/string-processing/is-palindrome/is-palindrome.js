bd91a2482dba25e59b1b475bb30f65a23329e4a7
JCPedroza <nemesis4go10@hotmail.com>
2022-01-22 22:18:52 -0600

    refa: add jsdoc

--- algorithms-and-data-structures/algorithms/string-processing/is-palindrome/is-palindrome.js
+++ algorithms-and-data-structures/algorithms/string-processing/is-palindrome/is-palindrome.js
@@ -4,6 +4,13 @@ const normalize = (str) => str.replace(tokensToRemove, '').toLowerCase()
 
 const reverse = (str) => [...str].reverse().join('')
 
+/**
+ * Checks if a string is a palindrome.
+ * Time complexity: O(n) for best, worst, average.
+ * Space complexity: O(n)
+ * @param {string} str String to be checked.
+ * @returns {boolean} True if the string is a palindrome, False otherwise.
+ */
 const isPalindrome = (str) => {
   const normalStr = normalize(str)
   return normalStr === reverse(normalStr)
@@ -11,5 +18,5 @@ const isPalindrome = (str) => {
 
 module.exports = {
   algorithm: isPalindrome,
-  id: 'string spread'
+  id: 'simple'
 }
