54b18156dd35940511295c4eb3eb541d2566e095
JCPedroza <nemesis4go10@hotmail.com>
2022-02-08 06:06:31 -0600

    feat (algo): test, implement, document binary search

---
+++ algorithms-and-data-structures/algorithms/searching/binary-search/binary-search.js
@@ -0,0 +1,32 @@
+/**
+ * Find the index of a value inside an array.
+ * Time complexity: O(1) best O(log n) worst O(log n).
+ * Space complexity: O(1).
+ * @param {*[]} array Sorted array containing the value to be found.
+ * @param {*} searchValue Value to find inside the array.
+ * @returns {number} Index of the value in the array, or -1 if value is
+ * not found.
+ */
+const binarySearch = (array, searchValue) => {
+  let leftLimit = 0
+  let rightLimit = array.length - 1
+
+  while (leftLimit <= rightLimit) {
+    const midIndex = Math.trunc((leftLimit + rightLimit) / 2)
+
+    if (array[midIndex] === searchValue) {
+      return midIndex
+    } else if (array[midIndex] < searchValue) {
+      leftLimit = midIndex + 1
+    } else {
+      rightLimit = midIndex - 1
+    }
+  }
+
+  return -1
+}
+
+module.exports = {
+  algorithm: binarySearch,
+  id: 'simple'
+}
---
+++ algorithms-and-data-structures/algorithms/searching/binary-search/binary-search.test.js
@@ -0,0 +1,12 @@
+const testSubjects = [
+  require('./binary-search')
+]
+
+for (const { algorithm, id } of testSubjects) {
+  test(`"${id}" finds index of item in array`, () => {
+    expect(algorithm([0, 1, 2, 3, 4], 2)).toBe(2)
+    expect(algorithm([0, 1, 2, 3, 4], 0)).toBe(0)
+    expect(algorithm([0, 1, 2, 3, 4], 4)).toBe(4)
+    expect(algorithm([0, 1, 2, 3, 4], 5)).toBe(-1)
+  })
+}
