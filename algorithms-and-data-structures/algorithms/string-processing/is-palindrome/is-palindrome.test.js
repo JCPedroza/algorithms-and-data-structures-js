173661123bc099d51869cc460fc830e2257b7af1
JCPedroza <nemesis4go10@hotmail.com>
2022-02-18 01:32:22 -0600

    refa: use describe-it-expect patter #10

--- algorithms-and-data-structures/algorithms/string-processing/is-palindrome/is-palindrome.test.js
+++ algorithms-and-data-structures/algorithms/string-processing/is-palindrome/is-palindrome.test.js
@@ -2,21 +2,18 @@ const testSubjects = [
   require('./is-palindrome')
 ]
 
-testSubjects.forEach(({ algorithm, id }) => {
-  const palindrome = `Anita! LAva :
+const palindrome = `Anita! LAva :
     la?; tina.`
 
-  const runPositiveTests = () => {
-    expect(algorithm(palindrome)).toBe(true)
-    expect(algorithm('')).toBe(true)
-    expect(algorithm('123321')).toBe(true)
-  }
+testSubjects.forEach(({ algorithm, id }) => {
+  describe(`Palindrome test algorithm "${id}"`, () => {
+    it('identifies palindrome strings', () => {
+      expect(algorithm(palindrome)).toBe(true)
+      expect(algorithm('')).toBe(true)
+      expect(algorithm('123321')).toBe(true)
 
-  const runNegativeTests = () => {
-    expect(algorithm('Not a palindrome :(')).toBe(false)
-    expect(algorithm('123123')).toBe(false)
-  }
-
-  test(`${id} recognizes palindromes`, runPositiveTests)
-  test(`${id} recognizes non-palindromes`, runNegativeTests)
+      expect(algorithm('Not a palindrome :(')).toBe(false)
+      expect(algorithm('123123')).toBe(false)
+    })
+  })
 })
