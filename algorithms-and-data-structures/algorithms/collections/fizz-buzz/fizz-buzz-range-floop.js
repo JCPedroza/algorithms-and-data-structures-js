dd75670ce4e770252e1006b15ee22473bdb56c71
JCPedroza <nemesis4go10@hotmail.com>
2022-02-19 15:31:53 -0600

    doc: include complexity analysis #17

---
+++ algorithms-and-data-structures/algorithms/collections/fizz-buzz/fizz-buzz-range-floop.js
@@ -0,0 +1,36 @@
+/**
+ * Converts a number into its equivalent fizz-buzz string.
+ * Time complexity: O(1)?
+ * Space complexity: O(1)?
+ * @param {number} num Number to convert.
+ * @returns {string} Fizz-buzz string equivalent to input number.
+ */
+const numToFizzBuzz = (num) => {
+  let fizzBuzz = ''
+  if (num % 3 === 0) fizzBuzz += 'fizz'
+  if (num % 5 === 0) fizzBuzz += 'buzz'
+  return fizzBuzz || String(num)
+}
+
+/**
+ * Generates a sequence of fizz-buzz strings.
+ * Time complexity: O(n).
+ * Space complexity: O(n).
+ * @param {number} from Start of the sequence.
+ * @param {number} to End of the sequence (inclusive).
+ * @returns {string[]} Sequence of fizz-buzz strings.
+ */
+const fizzBuzzRange = (from, to) => {
+  const sequence = []
+
+  for (let num = from; num <= to; num++) {
+    sequence.push(numToFizzBuzz(num))
+  }
+
+  return sequence
+}
+
+module.exports = {
+  algorithm: fizzBuzzRange,
+  id: 'for-loop accumulator'
+}
