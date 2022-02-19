ee8442803dd1824a535f1c7626ac29c1a2ed3bd3
JCPedroza <nemesis4go10@hotmail.com>
2022-02-19 15:33:21 -0600

    refa: use describe-it-expect pattern #10

--- algorithms-and-data-structures/algorithms/collections/fizz-buzz/fizz-buzz-range.test.js
+++ algorithms-and-data-structures/algorithms/collections/fizz-buzz/fizz-buzz-range.test.js
@@ -1,24 +1,21 @@
 const testSubjects = [
-  require('./fizz-buzz-range-forloop'),
-  require('./fizz-buzz-range-map')
+  require('./fizz-buzz-range-floop'),
+  require('./fizz-buzz-range-map'),
+  require('./fizz-buzz-range-recursive')
 ]
 
-const testCases = {
-  scenarioOne: {
-    in: [12, 20],
-    out: ['fizz', '13', '14', 'fizzbuzz', '16', '17', 'fizz', '19', 'buzz']
-  },
-
-  scenarioTwo: {
-    in: [-5, 3],
-    out: ['buzz', '-4', 'fizz', '-2', '-1', 'fizzbuzz', '1', '2', 'fizz']
-  }
-}
-
 testSubjects.forEach(({ algorithm, id }) => {
-  test(`"${id}" correctly generates fizzbuzz ranges`, () => {
-    Object.values(testCases).forEach(scenario => {
-      expect(algorithm(...scenario.in)).toEqual(scenario.out)
+  describe(`Fizz-buzz range algorithm "${id}"`, () => {
+    it('creates an array of fizz-buzz strings in the given range', () => {
+      expect(algorithm(12, 20))
+        .toEqual(['fizz', '13', '14', 'fizzbuzz', '16', '17', 'fizz', '19',
+          'buzz'])
+
+      expect(algorithm(-5, 3))
+        .toEqual(['buzz', '-4', 'fizz', '-2', '-1', 'fizzbuzz', '1', '2',
+          'fizz'])
+
+      expect(algorithm(0, 0)).toEqual(['fizzbuzz'])
     })
   })
 })
