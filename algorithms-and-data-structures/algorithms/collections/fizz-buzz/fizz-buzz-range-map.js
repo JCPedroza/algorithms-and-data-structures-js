59455b8d87346778ff7b2b026cf52da627543b93
JCPedroza <nemesis4go10@hotmail.com>
2022-02-19 15:33:46 -0600

    doc: include complexity analysis #17

--- algorithms-and-data-structures/algorithms/collections/fizz-buzz/fizz-buzz-range-map.js
+++ algorithms-and-data-structures/algorithms/collections/fizz-buzz/fizz-buzz-range-map.js
@@ -1,5 +1,7 @@
 /**
  * Generates a sequence of fizz-buzz strings.
+ * Time complexity: O(n).
+ * Space complexity: O(n).
  * @param {number} from Start of the sequence.
  * @param {number} to End of the sequence (inclusive).
  * @returns {string[]} Sequence of fizz-buzz strings.
@@ -8,8 +10,10 @@ const fizzBuzzRange = (from, to) =>
   [...Array(to - from + 1).keys()].map(index => {
     const num = index + from
     let fizzBuzz = ''
+
     if (num % 3 === 0) fizzBuzz += 'fizz'
     if (num % 5 === 0) fizzBuzz += 'buzz'
+
     return fizzBuzz || String(num)
   })
 
