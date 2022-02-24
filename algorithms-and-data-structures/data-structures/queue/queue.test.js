d685372a82e38a4c75b50354e0a3759de9025f48
JCPedroza <nemesis4go10@hotmail.com>
2022-02-23 19:59:41 -0600

    refa: use describe-it-expect pattern #10

--- algorithms-and-data-structures/data-structures/queue/queue.test.js
+++ algorithms-and-data-structures/data-structures/queue/queue.test.js
@@ -11,27 +11,29 @@ const validateState = (queue, length, isEmpty, peek) => {
   else expect(() => queue.peek()).toThrow(Error)
 }
 
-testSubjects.forEach(({ Structure, id }) =>
-  test(`"${id}" queue performs basic queue operations`, () => {
-    const queue = new Structure()
-    validateState(queue, 0, true)
+testSubjects.forEach(({ Structure, id }) => {
+  describe(`Queue data structure "${id}"`, () => {
+    it('performs basic queue operations', () => {
+      const queue = new Structure()
+      validateState(queue, 0, true)
 
-    expect(queue.enqueue('a')).toBe(queue)
-    validateState(queue, 1, false, 'a')
+      expect(queue.enqueue('a')).toBe(queue)
+      validateState(queue, 1, false, 'a')
 
-    queue.enqueue('b').enqueue('c').enqueue('d')
-    validateState(queue, 4, false, 'a')
+      queue.enqueue('b').enqueue('c').enqueue('d')
+      validateState(queue, 4, false, 'a')
 
-    expect(queue.dequeue()).toBe('a')
-    validateState(queue, 3, false, 'b')
+      expect(queue.dequeue()).toBe('a')
+      validateState(queue, 3, false, 'b')
 
-    expect(queue.dequeue()).toBe('b')
-    expect(queue.dequeue()).toBe('c')
-    expect(queue.dequeue()).toBe('d')
-    validateState(queue, 0, true)
+      expect(queue.dequeue()).toBe('b')
+      expect(queue.dequeue()).toBe('c')
+      expect(queue.dequeue()).toBe('d')
+      validateState(queue, 0, true)
 
-    expect(() => queue.peek()).toThrow(Error)
-    expect(() => queue.dequeue()).toThrow(Error)
-    expect(() => { queue.length = 5 }).toThrow(Error)
+      expect(() => queue.peek()).toThrow(Error)
+      expect(() => queue.dequeue()).toThrow(Error)
+      expect(() => { queue.length = 5 }).toThrow(Error)
+    })
   })
-)
+})
