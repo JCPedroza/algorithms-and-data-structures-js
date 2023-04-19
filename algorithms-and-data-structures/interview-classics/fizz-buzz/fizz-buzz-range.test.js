8634e11d28a5f170fe27a6ce9fd0704dae9c7eb7
JCPedroza <nemesis4go10@gmail.com>
2023-04-18 19:49:36 -0600

    test: use test runner instead of jest

--- algorithms-and-data-structures/interview-classics/fizz-buzz/fizz-buzz-range.test.js
+++ algorithms-and-data-structures/interview-classics/fizz-buzz/fizz-buzz-range.test.js
@@ -1,21 +1,25 @@
-const testSubjects = [
-  require('./fizz-buzz-range-floop'),
-  require('./fizz-buzz-range-map'),
-  require('./fizz-buzz-range-recursive')
-]
+const assert = require('node:assert/strict')
+const { describe, it } = require('node:test')
 
-testSubjects.forEach(({ algorithm, id }) => {
+const algorithms = require('./fizz-buzz.repo')
+
+algorithms.forEach(({ algorithm, id }) => {
   describe(`Fizz-buzz range algorithm "${id}"`, () => {
     it('creates an array of fizz-buzz strings in the given range', () => {
-      expect(algorithm(12, 20))
-        .toEqual(['fizz', '13', '14', 'fizzbuzz', '16', '17', 'fizz', '19',
-          'buzz'])
+      assert.deepEqual(
+        algorithm(12, 20),
+        ['fizz', '13', '14', 'fizzbuzz', '16', '17', 'fizz', '19', 'buzz']
+      )
 
-      expect(algorithm(-5, 3))
-        .toEqual(['buzz', '-4', 'fizz', '-2', '-1', 'fizzbuzz', '1', '2',
-          'fizz'])
+      assert.deepEqual(
+        algorithm(-5, 3),
+        ['buzz', '-4', 'fizz', '-2', '-1', 'fizzbuzz', '1', '2', 'fizz']
+      )
 
-      expect(algorithm(0, 0)).toEqual(['fizzbuzz'])
+      assert.deepEqual(
+        algorithm(0, 0),
+        ['fizzbuzz']
+      )
     })
   })
 })
---
+++ algorithms-and-data-structures/interview-classics/fizz-buzz/fizz-buzz.repo.js
@@ -0,0 +1,7 @@
+const algorithms = [
+  require('./fizz-buzz-range-floop'),
+  require('./fizz-buzz-range-map'),
+  require('./fizz-buzz-range-recursive')
+]
+
+module.exports = algorithms
