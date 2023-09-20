26e04a6f6e8b3d2b605df3e8bd6f38db447059fc
JCPedroza <nemesis4go10@gmail.com>
2023-09-19 19:52:48 -0600

    refa: group assertions vertically

--- algorithms-and-data-structures/algorithms/data-processing/object-property-sanitization/ops.test.js
+++ algorithms-and-data-structures/algorithms/data-processing/object-property-sanitization/ops.test.js
@@ -1,20 +1,33 @@
 const assert = require('node:assert/strict')
 const { describe, it } = require('node:test')
-
 const algorithms = require('./ops.repo')
 
 algorithms.forEach(({ fun, id }) => {
-  describe(`Object sanitization fun "${id}"`, () => {
-    it('sanitizes object properties according to a whitelist', () => {
+  describe(`Object sanitization algorithm "${id}"`, () => {
+    it('creates object containing whitelisted properties only', () => {
       const movie = { title: 'One', year: 1954, director: 'Tom' }
-      const movieWhitelist = ['title', 'year']
+      const whitelist = ['title', 'year']
 
-      assert.deepEqual(fun(movie, movieWhitelist), { title: 'One', year: 1954 })
-      assert.deepEqual(fun({}, movieWhitelist), {})
-      assert.deepEqual(fun(movie, []), {})
+      assert.deepEqual(
+        fun(movie, whitelist),
+        { title: 'One', year: 1954 }
+      )
+
+      assert.deepEqual(
+        fun({}, whitelist),
+        {}
+      )
+
+      assert.deepEqual(
+        fun(movie, []),
+        {}
+      )
 
       // Original object must not be modified
-      assert.deepEqual(movie, { title: 'One', year: 1954, director: 'Tom' })
+      assert.deepEqual(
+        movie,
+        { title: 'One', year: 1954, director: 'Tom' }
+      )
     })
   })
 })
