ae365544756e5862af513399af2b5b1ff4a41487
JCPedroza <nemesis4go10@hotmail.com>
2025-02-25 18:35:50 -0600

    conf: change default values

--- algorithms-and-data-structures/project-euler/001-multiples-of-3-or-5/multiples-of-3-or-5.time.js
+++ algorithms-and-data-structures/project-euler/001-multiples-of-3-or-5/multiples-of-3-or-5.time.js
@@ -1,8 +1,8 @@
 const solutions = require('./multiples-of-3-or-5.repo')
 const { timeAndReport } = require('../../../lib/time')
 
-const args = [10000]
-const runs = 200
+const args = [10_000]
+const runs = 1_000
 const id = 'Project Euler 001 - Multiples of 3 or 5'
 
 timeAndReport(solutions, args, runs, id)
