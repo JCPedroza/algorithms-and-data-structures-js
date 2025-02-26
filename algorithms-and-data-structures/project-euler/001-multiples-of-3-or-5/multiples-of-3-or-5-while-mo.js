7cbbd626c0b642aa23f8490f714c425d900cc000
JCPedroza <nemesis4go10@hotmail.com>
2025-02-25 23:15:19 -0600

    feat: implement while-loop optimization

---
+++ algorithms-and-data-structures/project-euler/001-multiples-of-3-or-5/multiples-of-3-or-5-while-mo.js
@@ -0,0 +1,36 @@
+/**
+ * Sum of multiples of 3 or 5 below 'limit'. Uses the while-loop accumulator
+ * pattern and trial division. Iterates through multiples only.
+ *
+ * Time complexity: O(n)
+ * Space complexity: O(1), the 64-bits IEEE 754 standard
+ *
+ * @param {number} limit Exclusive upper limit of the series.
+ * @returns {number} Sum of multiples of 3 or 5 below 'limit'.
+ */
+const sumMultsOf3Or5 = (limit) => {
+  const limitFloor = Math.floor(limit)
+  let mult3 = 3
+  let mult5 = 5
+  let sum = 0
+
+  while (mult5 < limitFloor) {
+    sum += mult3
+    if (mult5 % 3 !== 0) sum += mult5 // Avoid counting multiples of both twice
+
+    mult3 += 3
+    mult5 += 5
+  }
+
+  while (mult3 < limitFloor) {
+    sum += mult3
+    mult3 += 3
+  }
+
+  return sum
+}
+
+module.exports = {
+  fun: sumMultsOf3Or5,
+  id: 'while-loop multiples-only'
+}
--- algorithms-and-data-structures/project-euler/001-multiples-of-3-or-5/multiples-of-3-or-5.repo.js
+++ algorithms-and-data-structures/project-euler/001-multiples-of-3-or-5/multiples-of-3-or-5.repo.js
@@ -1,7 +1,8 @@
 const solutions = [
   require('./multiples-of-3-or-5-analytic'),
-  require('./multiples-of-3-or-5-forloop-obo'),
-  require('./multiples-of-3-or-5-reduce')
+  require('./multiples-of-3-or-5-for-obo'),
+  require('./multiples-of-3-or-5-reduce'),
+  require('./multiples-of-3-or-5-while-mo')
 ]
 
 module.exports = solutions
