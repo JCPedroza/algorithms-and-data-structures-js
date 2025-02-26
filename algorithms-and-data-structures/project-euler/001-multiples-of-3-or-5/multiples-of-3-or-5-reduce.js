fb7f6ec38d918c7c2bdb1c737688ac93c536898d
JCPedroza <nemesis4go10@hotmail.com>
2025-02-25 19:40:11 -0600

    refa: use floor for number truncation

--- algorithms-and-data-structures/project-euler/001-multiples-of-3-or-5/multiples-of-3-or-5-reduce.js
+++ algorithms-and-data-structures/project-euler/001-multiples-of-3-or-5/multiples-of-3-or-5-reduce.js
@@ -1,23 +1,21 @@
 /**
- * Sum of natural numbers multiples of 3 or 5 and below the limit.
- * Uses the filter-reduce pattern.
+ * Sum of multiples of 3 or 5 below 'limit'. Uses the filter-reduce pattern.
+ *
  * Time complexity: O(n)
  * Space complexity: O(n), the length of the range array
- * @param {number} limit Limit of the series.
- * @returns {number} Sum of natural numbers divisible by n and
- * below the limit.
+ *
+ * @param {number} limit Exclusive upper limit of the series.
+ * @returns {number} Sum of multiples of 3 or 5 below 'limit'.
  */
 const multiplesOf3Or5 = (limit) => {
   if (limit < 0) return 0
 
-  const intLimit = parseInt(limit)
-
-  return [...Array(intLimit).keys()] // [0, 1, ... , lim - 1]
-    .filter(num => num % 3 === 0 || num % 5 === 0)
-    .reduce((sum, num) => sum + num, 0)
+  return [...Array(Math.floor(limit)).keys()] // Array of non-negative integers
+    .filter(intNum => intNum % 3 === 0 || intNum % 5 === 0)
+    .reduce((sum, intNum) => sum + intNum, 0)
 }
 
 module.exports = {
   fun: multiplesOf3Or5,
-  id: 'filter reduce'
+  id: 'filter-reduce'
 }
