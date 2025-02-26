e600ca7430063f7b99a9b7a113add141530a9423
JCPedroza <nemesis4go10@hotmail.com>
2025-02-25 18:34:52 -0600

    test: add more comprehensive testing

--- algorithms-and-data-structures/project-euler/001-multiples-of-3-or-5/multiples-of-3-or-5.test.js
+++ algorithms-and-data-structures/project-euler/001-multiples-of-3-or-5/multiples-of-3-or-5.test.js
@@ -5,35 +5,26 @@ const solutions = require('./multiples-of-3-or-5.repo')
 
 for (const { fun, id } of solutions) {
   describe(`Project Euler "Multiples of 3 or 5" solution "${id}"`, () => {
-    it('sums multiples of 3 or 5 that are smaller than the input', () => {
-      assert.equal(
-        fun(0),
-        0
-      )
+    it('sums multiples that are valued less than a small input', () => {
+      assert.equal(fun(0), 0)
+      assert.equal(fun(1), 0)
+      assert.equal(fun(2), 0)
+      assert.equal(fun(3), 0)
+      assert.equal(fun(4), 3)
+      assert.equal(fun(5), 3)
+      assert.equal(fun(6), 8)
+    })
 
-      assert.equal(
-        fun(10),
-        23
-      )
-
-      assert.equal(
-        fun(100),
-        2_318
-      )
+    it('sums multiples that are valued less than a medium input', () => {
+      assert.equal(fun(1_000), 233_168)
     })
 
     it('returns 0 for negative input', () => {
-      assert.equal(
-        fun(-10),
-        0
-      )
+      assert.equal(fun(-10), 0)
     })
 
-    it('converts numerical input to integer', () => {
-      assert.equal(
-        fun(15.9),
-        fun(15)
-      )
+    it('treats fractional input as integer', () => {
+      assert.equal(fun(15.9), fun(15))
     })
   })
 }
