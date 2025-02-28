fc724ab04ed4214dad9cff9fd448fb43ac74f7cb
JCPedroza <nemesis4go10@hotmail.com>
2025-02-28 14:46:59 -0600

    fix: create unwrapper for type

--- src/algorithms/sorting/merge-sort/merge-sort.js
+++ src/algorithms/sorting/merge-sort/merge-sort.js
@@ -1,14 +1,26 @@
 /**
+ * Unpacks 'number | undefined' type into 'number'
+ *
+ * @private
+ * @param {number[]} array Numeric array.
+ * @returns {number} Shifted array value.
+ */
+const shift = (array) => array.shift() ?? 0
+
+/**
  * Merges two ordered arrays into one ordered array.
+ *
+ * @private
  * @param {number[]} left One of the arrays to be merged.
  * @param {number[]} right One of the arrays to be merged.
  * @returns {number[]} Merged array.
  */
-const merge = (left, right) => {
+const merge = (left = [], right = []) => {
+  /** @type {number[]} */
   const sorted = []
 
   while (left.length && right.length) {
-    sorted.push(left[0] <= right[0] ? left.shift() : right.shift())
+    sorted.push(left[0] <= right[0] ? shift(left) : shift(right))
   }
 
   return [...sorted, ...left, ...right]
@@ -16,8 +28,10 @@ const merge = (left, right) => {
 
 /**
  * Sorts a numeric array using the merge sort approach.
+ *
  * Time complexity: O(n log n) for best, worst, and average.
  * Space complexity: total O(n) auxiliary O(n).
+ *
  * @param {number[]} nums Array to be sorted.
  * @returns {number[]} Sorted array.
  */
