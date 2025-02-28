5579293aa53d6ccebad4b1360a82fc65ade84d62
JCPedroza <nemesis4go10@hotmail.com>
2025-02-28 14:14:31 -0600

    fix: add types to inner function

--- src/project-euler/002-even-fibonacci-numbers/even-fibo-tail.js
+++ src/project-euler/002-even-fibonacci-numbers/even-fibo-tail.js
@@ -1,13 +1,23 @@
 /**
  * Calculates the sum of the even fibonacci numbers, up to the given limit.
  * Uses tail recursion and analytic optiization.
+ *
  * Time complexity: O(n)
  * Space complexity: O(n) ? (tail recursion)
+ *
  * @param {number} limit Max value of the fibonacci numbers (inclusive).
  * @returns {number} Sum of the even fibonacci numbers that do not exceed the
  * limit.
  */
 const fiboEvenSum = (limit) => {
+  /**
+   * Helper recursive function.
+   *
+   * @param {number} acc Accumulated sum.
+   * @param {number} fib Current fibo number.
+   * @param {number} nxtFib Next fibo number.
+   * @returns {number} Sum of the even fibo numbers below the limit.
+   */
   const loop = (acc, fib, nxtFib) => {
     if (fib > limit) return acc
     return loop(acc + fib, nxtFib, 4 * nxtFib + fib)
