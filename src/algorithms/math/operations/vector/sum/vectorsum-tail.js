f4b0988285b79b95179a02a30cb575230cdffaed
JCPedroza <nemesis4go10@hotmail.com>
2025-02-28 14:52:31 -0600

    fix: write types for inner function

--- src/algorithms/math/operations/vector/sum/vectorsum-tail.js
+++ src/algorithms/math/operations/vector/sum/vectorsum-tail.js
@@ -1,9 +1,17 @@
 /**
  * Sum the numbers in an array using tail recursion.
- * @param {number[]} nums Array of numbers.
+ *
+ * @param {number[]} nums Array of numbers to sum.
  * @returns {number} Sum of the numbers in the array.
  */
 const vsum = (nums) => {
+  /**
+   * Helper recursive function.
+   *
+   * @param {number[]} nums Array of numbers to sum.
+   * @param {number} sum Currently accumulated sum.
+   * @returns {number} Total accumulated sum.
+   */
   const loop = (nums, sum = 0) => {
     if (nums.length) {
       const [head, ...tail] = nums
